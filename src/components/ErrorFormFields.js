681ba09b5f31caecef3cc2478afa9225489c4634
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-10-01 10:57:33 -0500

    Se separa formError en un componente nuevo llamado FormErrorFields

---
+++ src/components/ErrorFormFields.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ErrorFormFields = () => {
+    return (
+        <div className='alert alert-danger' role='alert'>
+            ALL FIELDS ARE REQUIRED
+        </div>
+    );
+};
