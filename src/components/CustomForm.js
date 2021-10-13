525dfb8e27a43f2ec96c633333b48c54aa6f989b
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-10-12 19:49:47 -0500

    Quitando Fragment de CustomForm.js ya que no es necesario

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -17,57 +17,55 @@ export const CustomForm = ({
         useCardOptionsColor(setValues);
 
     return (
-        <>
-            <div className='mt-2 col pb-3'>
-                <TodoTitle />
+        <div className='mt-2 col pb-3'>
+            <TodoTitle />
 
-                <form className='mx-3 my-3' onSubmit={handleSubmit}>
-                    <div className='d-flex justify-content-center'>
-                        {formError && <ErrorFormFields />}
-                    </div>
+            <form className='mx-3 my-3' onSubmit={handleSubmit}>
+                <div className='d-flex justify-content-center'>
+                    {formError && <ErrorFormFields />}
+                </div>
 
-                    <div className='d-flex'>
-                        <input
-                            className='form-control mb-2 me-3 rounded-pill shadow-lg'
-                            type='text'
-                            name='todo'
-                            placeholder='To Do...'
-                            autoComplete='off'
-                            onChange={handleInputChange}
-                            value={todo}
+                <div className='d-flex'>
+                    <input
+                        className='form-control mb-2 me-3 rounded-pill shadow-lg'
+                        type='text'
+                        name='todo'
+                        placeholder='To Do...'
+                        autoComplete='off'
+                        onChange={handleInputChange}
+                        value={todo}
+                    />
+                    <button
+                        className='btn btn-warning mb-2 rounded-pill border-4'
+                        type='submit'
+                    >
+                        Add
+                    </button>
+                </div>
+
+                <div>
+                    <input
+                        className='form-control mb-2 rounded-pill'
+                        type='datetime-local'
+                        name='dateTime'
+                        onChange={handleInputChange}
+                        value={dateTime}
+                    />
+                </div>
+
+                <div className='row text-center mt-4 d-flex justify-content-center'>
+                    {colors.map((color, index) => (
+                        <OptionColor
+                            key={index + new Date().getTime()}
+                            index={index}
+                            color={color}
+                            colors={colors}
+                            setColors={setColors}
+                            handleChangeFocus={handleChangeFocus}
                         />
-                        <button
-                            className='btn btn-warning mb-2 rounded-pill border-4'
-                            type='submit'
-                        >
-                            Add
-                        </button>
-                    </div>
-
-                    <div>
-                        <input
-                            className='form-control mb-2 rounded-pill'
-                            type='datetime-local'
-                            name='dateTime'
-                            onChange={handleInputChange}
-                            value={dateTime}
-                        />
-                    </div>
-
-                    <div className='row text-center mt-4 d-flex justify-content-center'>
-                        {colors.map((color, index) => (
-                            <OptionColor
-                                key={index + new Date().getTime()}
-                                index={index}
-                                color={color}
-                                colors={colors}
-                                setColors={setColors}
-                                handleChangeFocus={handleChangeFocus}
-                            />
-                        ))}
-                    </div>
-                </form>
-            </div>
-        </>
+                    ))}
+                </div>
+            </form>
+        </div>
     );
 };
