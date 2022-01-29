2431cf2abf45952efd032c4cde8f9b0c8bdc228c
Nicolay Moreno <nicolay.mherrera@gmail.com>
2022-01-28 19:24:15 -0500

    test readme-md

---
+++ README.md
@@ -0,0 +1,5 @@
+<div>
+<p style = 'text-align:center;'>
+<img src="./imagesReadme/sreenshot.PNG" alt="todo-list-img" width="400px">
+</p>
+</div>
--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -13,7 +13,7 @@ export const CustomForm = ({
     alertState,
     alertColor,
     colors,
-    setColors,
+    setColors, 
     handleChangeFocus,
     colorTitle,
     colorButtonAdd,
